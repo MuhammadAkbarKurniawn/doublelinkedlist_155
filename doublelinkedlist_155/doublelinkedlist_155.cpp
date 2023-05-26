1835687dddfb244064b3b4d72dd6c1895c5dc4bd
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-05-26 11:00:54 +0700

    Add project files.

---
+++ doublelinkedlist_155/doublelinkedlist_155.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Node {
+public:
+	int noMhs;
+	string nama;
+	Node* next;
+	Node* prev;
+};
+
+class Doublelinkedlist {
+private:
+	Node* START;
+public:
+	Doublelinkedlist();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
+
+};
